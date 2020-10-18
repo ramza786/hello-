539544260b6f7a80e58f3944f71803abc2081d64
RAMZA <ramzashahzadi786@gmail.com>
2020-10-18 16:45:49 +0500

    forloop.java

---
+++ method.java
@@ -0,0 +1,21 @@
+// C++ code to show not using return 
+// statement in void return type function 
+
+#include <iostream> 
+using namespace std; 
+
+// void method 
+void Print() 
+{ 
+	printf("Ramza Shahzadi"); 
+} 
+
+// Driver method 
+int main() 
+{ 
+
+	// Calling print 
+	Print(); 
+
+	return 0; 
+} 
