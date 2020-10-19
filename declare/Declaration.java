85f13a073d01bf2dcfcf1278b32ce796ac7b9d47
Ramza <ramzashahzadi786@gmail.com>
2020-10-19 18:43:56 +0500

    declaration.java

---
+++ declare/Declaration.java
@@ -0,0 +1,9 @@
+class Test
+{
+  public static void main ( String[] args )
+  {
+    long payAmount = 123;    //the declaration of the variable
+
+    System.out.println("The variable contains: " + payAmount );
+  }
+}
