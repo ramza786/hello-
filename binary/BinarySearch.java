cbf388f9f9486786ffe435ee443f45028a94189f
Ramza <ramzashahzadi786@gmail.com>
2020-10-26 19:39:14 +0500

    BinarySearch.java

---
+++ binary/BinarySearch.java
@@ -0,0 +1,70 @@
+
+import java.util.Arrays;
+
+
+public class BinarySearch 
+{
+
+    private BinarySearch()
+	 {
+ 
+   		 public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+  
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+
+  
+    public static void main(String[] args) {
+
+        // read the integers from a file
+        In in = new In(args[0]);
+        int[] allowlist = in.readAllInts();
+
+        // sort the array
+        Arrays.sort(allowlist);
+
+        // read integer key from standard input; print if not in allowlist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(allowlist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
+
+ *  Compilation:  javac BinarySearch.java
+ *  Execution:    java BinarySearch allowlist.txt < input.txt
+ *  Dependencies: In.java StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/11model/tinyW.txt
+ *                https://algs4.cs.princeton.edu/11model/tinyT.txt
+ *                https://algs4.cs.princeton.edu/11model/largeW.txt
+ *                https://algs4.cs.princeton.edu/11model/largeT.txt
+ *
+ *  % java BinarySearch tinyW.txt < tinyT.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % java BinarySearch largeW.txt < largeT.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *  
+ ******************************************************************************/
